25a2d0015db6164301f2043cb856ac2f37b76b9a
Lucas Zeer <lucaszw@v1020-wn-41-165.campus-dynamic.uwaterloo.ca>
2016-10-29 19:17:37 -0400

    added square helper

---
+++ Assets/Square/Square.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Square : MonoBehaviour {
+	public GameObject selector;
+	private int gridX, gridY, helperID;
+
+	public void ShowHelper() {
+		// Show Helper Square (To hint where the new obj will be placed):
+		float x = this.transform.position.x;
+		float z = this.transform.position.z;
+
+		Object obj = Instantiate (selector, new Vector3 (x, 0.5f, z), Quaternion.identity);
+		helperID = obj.GetInstanceID ();
+	}
+
+	public int GetHelperID(){
+		return this.helperID;
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
