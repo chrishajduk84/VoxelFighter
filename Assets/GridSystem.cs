27ccd3e84756ad0c87de63911f1aba619487660e
Chris Hajduk <chrishajduk84@gmail.com>
2016-10-29 20:18:26 -0400

    Added the ability to move the camera around the stage

--- Assets/GridSystem.cs
+++ Assets/GridSystem.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class GridSystem : MonoBehaviour {
 	public GameObject square;
 
 	const float gridSpacing = 1.2f;
-	// Use this for initialization
+    const int X_MAX = 6;
+    const int Z_MAX = 6;
+    const float FOV = 32;
+    const float ASPECT_RATIO = 2;
+	
+    // Use this for initialization
 	void Start () {
 
-		this.transform.position = new Vector3(0,this.transform.position.y,0);
+        float cDistanceX = (gridSpacing * X_MAX) / (2 * Mathf.Tan(FOV * Mathf.Deg2Rad));
+        float cDistanceZ = (gridSpacing * Z_MAX) / (2 * Mathf.Tan(FOV * Mathf.Deg2Rad / ASPECT_RATIO));
+        float theta = 45; float phi = 45; //Starting Camera Angle 
 
-		for (int u = 0; u < 6; u++) {
-			for (int v = 0; v < 6; v++) {
-				Instantiate(square, new Vector3(gridSpacing*u, 0f, gridSpacing*v), Quaternion.identity);
+        //Camera Angle
+        this.transform.eulerAngles = new Vector3(phi, theta, 0);//phi * Mathf.Sin(theta * Mathf.Deg2Rad)
+        Debug.Log(this.transform.eulerAngles.ToString());
+
+        //Center of Stage
+        Vector3 stageCenter = new Vector3(X_MAX * gridSpacing/2, 0, Z_MAX * gridSpacing/2);
+        Debug.Log(stageCenter.ToString());
+        //Camera Distance
+        //Vector3 camCenter = new Vector3(cDistanceX * Mathf.Cos(theta * Mathf.Deg2Rad)*Mathf.Cos(phi * Mathf.Deg2Rad) + cDistanceZ * Mathf.Sin(theta * Mathf.Deg2Rad)*Mathf.Cos(phi * Mathf.Deg2Rad), cDistanceZ * Mathf.Sin(phi*Mathf.Deg2Rad), cDistanceX * Mathf.Sin(theta * Mathf.Deg2Rad) * Mathf.Cos(phi * Mathf.Deg2Rad) + cDistanceZ * Mathf.Cos(theta * Mathf.Deg2Rad) * Mathf.Cos(phi * Mathf.Deg2Rad));
+        Vector3 camCenter = new Vector3(cDistanceZ * Mathf.Sin(-theta * Mathf.Deg2Rad) * Mathf.Cos(phi * Mathf.Deg2Rad), cDistanceZ * Mathf.Sin(phi * Mathf.Deg2Rad), -cDistanceZ * Mathf.Cos(-theta * Mathf.Deg2Rad) * Mathf.Cos(phi * Mathf.Deg2Rad));
+
+        //Total Position
+        this.transform.position = stageCenter + camCenter;
+
+		for (int u = 0; u < X_MAX; u++) {
+			for (int v = 0; v < Z_MAX; v++) {
+				Instantiate(square, new Vector3(gridSpacing*u + square.transform.localScale.x/2, 0f, gridSpacing* v + square.transform.localScale.z / 2), Quaternion.identity);
 
 			}
 		}
